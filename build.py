3e6d3951eec104bff8c4f22f917018ccf144fbd1
gabriel quesnot <gaqu1994@gmail.com>
2022-07-04 22:08:42 +0200

    remove build.py

--- build.py
+++
@@ -1,5 +0,0 @@
-from src.controllers.schema_root import SchemaRoot
-
-if __name__ == "__main__":
-    schema_root = SchemaRoot(json_path="test_jsons", dtc_path="test_dataclass")
-    schema_root.generate("champions")
